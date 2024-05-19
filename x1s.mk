c208cc385eb24ddfca431b3e8f26bfb4a33ff12d
oberdfr <alebennella@gmail.com>
2024-05-19 20:07:47 +0200

    x1s: adapt to HentaiOS

--- x1s.mk
+++ x1s.mk
@@ -14,20 +14,18 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Non AB device 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 # Inherit from x1s device
 $(call inherit-product, device/samsung/x1s/device.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common HentaiOS stuff.
+$(call inherit-product, device/samsung/universal9830-common/hentai-common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x1s
-PRODUCT_NAME := lineage_x1s
+PRODUCT_NAME := x1s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G981B
 PRODUCT_MANUFACTURER := samsung
@@ -35,14 +33,3 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
 TARGE_BUILD_LINEAGEHW := false
-
-# Use the latest approved GMS identifiers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=x1sxxx \
-    PRIVATE_BUILD_DESC="x1sxxx-user 11 RP1A.200720.012 G981BXXSJHXC1 release-keys"
-
-BUILD_FINGERPRINT := samsung/x1sxxx/x1s:11/RP1A.200720.012/G981BXXSJHXC1:user/release-keys
-
-# Define PDA property for camera
-PRODUCT_SYSTEM_PROPERTIES += \
-    ro.build.PDA=G981BXXSJHXC1
